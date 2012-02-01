63f303184e94d800082b0ec6a7c3f41a6e38bf83
Kurt <kespindler@gmail.com>
2012-01-31 22:55:06 -0800

    Initial Commit

---
+++ CirclesApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  CirclesApp
+//
+//  Created by Kurt Spindler on 1/31/12.
+//  Copyright (c) 2012 Brown University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ CirclesApp/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CirclesApp
+//
+//  Created by Kurt Spindler on 1/31/12.
+//  Copyright (c) 2012 Brown University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
